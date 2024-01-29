73e03424d5e7ac0fc58aba82e193e1f6da1284ef
fartunxsaid <s374964@oslomet.no>
2024-01-29 18:21:43 +0100

    lagt til sikkhets test

--- Nettbank/src/test/java/oslomet/testing/EnhetstestSikkerhet.java
+++ Nettbank/src/test/java/oslomet/testing/EnhetstestSikkerhet.java
@@ -1,4 +1,28 @@
 package oslomet.testing;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpSession;
+import oslomet.testing.DAL.BankRepository;
+import oslomet.testing.Sikkerhet.Sikkerhet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+@RunWith(MockitoJUnitRunner.class)
 public class EnhetstestSikkerhet {
+    @InjectMocks
+    // denne skal testes
+    private Sikkerhet sikkerhetsController;
+
+    @Mock
+    // denne skal Mock'es
+    private BankRepository repository;
+
+    @Mock
+    // denne skal Mock'es
+    private MockHttpSession session;
+
 }
