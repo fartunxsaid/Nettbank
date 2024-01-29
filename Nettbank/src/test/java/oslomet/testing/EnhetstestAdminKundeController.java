3cfd40b191057ad35f50a8ab18c728fd9c4eee8e
fartunxsaid <s374964@oslomet.no>
2024-01-29 16:04:52 +0100

    lagt test for Endre OK og ikke OK

--- Nettbank/src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
+++ Nettbank/src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
@@ -82,5 +82,28 @@ public class EnhetstestAdminKundeController {
         //assert
         assertEquals("Ikke logget inn",resultat);
     }
+    @Test
+    public void test_EndreOK(){
+        // arrange
+        Kunde kunde1= new Kunde("01010110523","Lene","Jensen","Askerveien 22","3270","22224444","HeiHei");
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+        when(repository.registrerKunde(kunde1)).thenReturn("OK");
+        //act
+        String resultat = AdminKundeController.lagreKunde(kunde1);
+        //assert
+        assertEquals("OK", resultat);
+    }
+    @Test
+    public void test_EndreIkkeOK(){
+        // arrange
+        Kunde kunde1= new Kunde("01010110523","Lene","Jensen","Askerveien 22","3270","22224444","HeiHei");
+        when(sjekk.loggetInn()).thenReturn(null);
+        //act
+        String resultat = AdminKundeController.lagreKunde(kunde1);
+        //assert
+        assertEquals("Ikke logget inn",resultat);
+    }
+
+
 }
 
