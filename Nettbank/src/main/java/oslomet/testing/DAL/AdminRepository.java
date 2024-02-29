711e75444d21649df7273d5bf95867c66ef7960a
emelieminin <s374930@oslomet.no>
2024-02-29 11:49:24 +0100

    lagt till kode i repository och controller

--- Nettbank/src/main/java/oslomet/testing/DAL/AdminRepository.java
+++ Nettbank/src/main/java/oslomet/testing/DAL/AdminRepository.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
 import oslomet.testing.Models.Konto;
 import oslomet.testing.Models.Kunde;
 import javax.sql.DataSource;
@@ -172,8 +173,7 @@ public class AdminRepository {
         try{
             Resource skjema = new ClassPathResource("schema.sql");
             Resource data = new ClassPathResource("data.sql");
-            ResourceDatabasePopulator databasePopulator = new
-                    ResourceDatabasePopulator(skjema,data);
+            ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator(skjema,data);
             databasePopulator.execute(dataSource);
             return "OK";
         }
